f9438f2ea0f0a4a3bd9f3411e79a3bb87f7ef9fc
mokamint <tls64b@gmail.com>
2022-02-14 11:21:03 +0900

    UI 사용성 개선 및 잘못된 URL에 대해 예외처리

--- app.js
+++ app.js
@@ -13,22 +13,30 @@ app.get('/', (req, res) => {
 
 app.get('/audio', async (req, res) => {
     var url = req.query.url;
+    if (!url.includes('youtube') && !url.includes('youtu.be')) {
+        res.send("URL 오류");
+        return;
+    }
     try { 
         await ytdl(url, {filter:'audioonly'}).pipe(fs.createWriteStream(__dirname + '/files/audio.mp3')).on('finish', () => {
             res.redirect('/static/audio.mp3');
         });
     }
-    catch { res.send('오류, 나중에 다시 시도 해 주세요.'); }
+    catch { res.send('오류. 나중에 다시 시도 해 주세요.'); }
 })
 
 app.get('/video', async (req, res) => {
     var url = req.query.url;
+    if (!url.includes('youtube') && !url.includes('youtu.be')) {
+        res.send("URL 오류");
+        return;
+    }
     try { 
         await ytdl(url, {filter:'videoandaudio', quality: 'highestvideo'}).pipe(fs.createWriteStream(__dirname + '/files/video.mp4')).on('finish', () => {
             res.redirect('/static/video.mp4');
         });
     }
-    catch { res.send('오류, 나중에 다시 시도 해 주세요.'); }
+    catch { res.send('오류. 나중에 다시 시도 해 주세요.'); }
 })
 
 app.listen(port);
