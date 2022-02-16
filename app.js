9a1c9ce6a1057758ff7db4512674cc2bc30398c3
mokamint <tls64b@gmail.com>
2022-02-16 10:22:05 +0900

    UI 개선

--- app.js
+++ app.js
@@ -9,34 +9,36 @@ app.use('/static', express.static(__dirname + '/files'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
-})
+});
 
 app.get('/audio', async (req, res) => {
+    var filename = "audio.mp3";
     var url = req.query.url;
     if (!url.includes('youtube') && !url.includes('youtu.be')) {
         res.send("URL 오류");
         return;
     }
     try { 
-        await ytdl(url, {filter:'audioonly'}).pipe(fs.createWriteStream(__dirname + '/files/audio.mp3')).on('finish', () => {
-            res.redirect('/static/audio.mp3');
+        await ytdl(url, {filter:'audioonly', quality: 'lowest'}).pipe(fs.createWriteStream(__dirname + '/files/' + filename)).on('finish', () => {  
+            res.redirect('/static/' + filename);
         });
     }
     catch { res.send('오류. 나중에 다시 시도 해 주세요.'); }
-})
+});
 
 app.get('/video', async (req, res) => {
+    var filename = "video.mp4";
     var url = req.query.url;
     if (!url.includes('youtube') && !url.includes('youtu.be')) {
         res.send("URL 오류");
         return;
     }
     try { 
-        await ytdl(url, {filter:'videoandaudio', quality: 'highestvideo'}).pipe(fs.createWriteStream(__dirname + '/files/video.mp4')).on('finish', () => {
-            res.redirect('/static/video.mp4');
+        await ytdl(url, {filter:'videoandaudio', quality: 'highestvideo'}).pipe(fs.createWriteStream(__dirname + '/files/' + filename)).on('finish', () => {
+            res.redirect('/static/' + filename);
         });
     }
     catch { res.send('오류. 나중에 다시 시도 해 주세요.'); }
-})
+});
 
 app.listen(port);
